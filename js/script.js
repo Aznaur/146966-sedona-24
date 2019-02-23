027b6182ad2bfbe2c38db2e28b8a02bd3c4fa5f8
Aznaur <40858331+Aznaur@users.noreply.github.com>
2019-02-23 17:56:43 +0300

    Доработал js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 var link = document.querySelector(".booking-search");
 var popup =  document.querySelector(".hotel-search-wrapper");
-link.addEventListener("click", function(evt) {
 
-  evt.preventDefault();
+if (link) {
+  if (popup) {
+    popup.classList.add("modal-hide");
+  };
 
-  popup.classList.toggle("modal-show")
-})
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.toggle("modal-hide");
+    popup.classList.toggle("modal-show");
+  });
+};
